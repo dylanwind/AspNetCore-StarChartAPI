e5bd9db98d8afd206dbd4f118b8334c513ceba24
Dylan Wang <dylanwang9@gmail.com>
2019-01-13 11:38:30 -0800

    Create CelestialObjectContrlller's Get Actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -17,6 +18,55 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext dbContext)
         {
             _context = dbContext;
+        }
+
+        [HttpGet("{id:int}",Name ="GetById")]
+        public IActionResult GetById(int id)
+        {
+            var res = _context.CelestialObjects.FirstOrDefault(e => e.Id == id);
+           
+
+            if (res == null)
+            {
+                return NotFound();
+            }
+
+            var satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == id);
+            res.Satellites = satellites == null ? null : satellites.ToList();
+
+            return Ok(res);
+        }
+
+        [HttpGet("{name}", Name = "GetByName")]
+        public IActionResult GetByName(string name)
+        {
+            var res = _context.CelestialObjects.FirstOrDefault(e => e.Name == name);
+
+
+            if (res == null)
+            {
+                return NotFound();
+            }
+
+            var satellites = _context.CelestialObjects.Where(e => e.Id == res.Id);
+            res.Satellites = satellites == null ? null : satellites.ToList();
+
+            return Ok(res);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestialObjs = _context.CelestialObjects.ToList();
+
+            foreach (CelestialObject celestial
+                in celestialObjs)
+            {
+                var satellites = _context.CelestialObjects.Where(e => e.Id == celestial.Id).ToList();
+                celestial.Satellites = satellites;
+            }
+
+            return Ok(celestialObjs);
         }
     }
 }
